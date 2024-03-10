31dc9a792db2d659c27cc7831f6d41ac3f9214b8
Hyderite <hi@hyderite.dev>
2024-03-11 01:42:21 +0800

    Added main-2

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const main2Title = document.querySelector("#main-2-title"), main2Text = document.querySelector("#main-2-text"), verticalRect = document.querySelector("#vertical-rect"), horizontalRect = document.querySelector("#horizontal-rect"), main2Image = document.querySelector("#main-2-image")
+var scrollOffset = 100;
+
+const elementInView = (el, offset = 0) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        ((window.innerHeight || document.documentElement.clientHeight) - offset)
+    );
+};
+
+const displayScrollElement = () => {
+    main2Title.classList.add("main-2-scrolled");
+    main2Text.classList.add("main-2-scrolled");
+    verticalRect.style.setProperty("--vertical-rect-animation", "expand-vertical-rect .5s");
+    verticalRect.style.setProperty("--rect-animation-fill-mode", "forwards");
+    verticalRect.style.setProperty("--vertical-rect-animation-delay", ".65s");
+    horizontalRect.style.setProperty("--horizontal-rect-animation", "expand-horizontal-rect .5s");
+    horizontalRect.style.setProperty("--rect-animation-fill-mode", "forwards");
+    horizontalRect.style.setProperty("--horizontal-rect-animation-delay", ".95s");
+    main2Image.classList.add("main-2-scrolled");
+};
+
+const handleScrollAnimation = () => {
+    if (elementInView(main2Title, scrollOffset)) {
+        displayScrollElement();
+    };
+};
+
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+});
