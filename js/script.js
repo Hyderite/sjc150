624a7188de31e2f2f69b61c84d6f6c11d1eb4c32
Hyderite <hi@hyderite.dev>
2024-03-10 23:42:09 +0800

    Added taskbar scrolling animation

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function () {
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+        document.querySelector("#taskbar").style.marginTop = "-80px";
+    } else if (st < lastScrollTop) {
+        document.querySelector("#taskbar").style.marginTop = "0";
+    };
+    lastScrollTop = st <= 0 ? 0 : st;
+}, false);
