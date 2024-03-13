6a64a770b0f9df3799b06b35838828144813ee5e
Hyderite <hi@hyderite.dev>
2024-03-13 14:08:06 +0800

    removed comment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var lastScrollTop = 0;
 
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
 window.addEventListener("scroll", function () {
     var st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScrollTop) {
