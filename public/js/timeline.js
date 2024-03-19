2250905f6db1b5c37930a34f6393c21f2430dda2
Hyderite <hi@hyderite.dev>
2024-03-19 23:18:42 +0800

    changed background

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,15 +1,17 @@
-document.querySelector("#sjc-img").style.animation = "enlarge .75s forwards";
-setTimeout(function () {
-    document.querySelector("#sjc-img").style.animation = "move-up .75s forwards";
-}, 1250);
+function animation() {
+    document.querySelector("#sjc-img").style.animation = "enlarge .75s forwards";
+    setTimeout(function () {
+        document.querySelector("#sjc-img").style.animation = "move-up .75s forwards";
+    }, 1400);
+};
+
+animation();
+document.querySelector("#main").style.transform = "translateY(" + window.pageYOffset + document.querySelector("#sjc-img").getBoundingClientRect().top + "px)";
 
 document.addEventListener("visibilitychange", () => {
     if (window.scrollY == 0) {
         if (document.visibilityState === "visible") {
-            document.querySelector("#sjc-img").style.animation = "enlarge .75s forwards";
-            setTimeout(function () {
-                document.querySelector("#sjc-img").style.animation = "move-up .75s forwards";
-            }, 1250);
+            animation();
         } else {
             document.querySelector("#sjc-img").style.animation = "none";
         };
