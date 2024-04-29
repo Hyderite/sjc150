e1fe7a529a257fc1a1408897159ec2e4e1484436
Ernest Wong <hi@hyderite.dev>
2024-04-30 01:26:01 +0800

    changed color + font, home page animation rework undone, timeline undone

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,38 @@
-document.querySelector("#box-1").style.animation = "enlarge .75s forwards";
+let navbar = document.querySelector("#navbar");
+
+navbar.style.transform = "translateY(-160px)";
+document.querySelector("#navbar-bg").style.transform = "translateY(-160px)";
+document.querySelector("#main-1").style.marginTop = "0";
+document.querySelector("#main-1").style.height = "100%";
+
+document.querySelector("#sjc-150-logo-loading").style.opacity = "1";
+document.querySelector("#sjc-150-logo-loading").style.marginTop = "0";
+setTimeout(function () {
+    document.querySelector("#loading-cross").style.opacity = "1";
+    document.querySelector("#loading-cross").style.marginTop = "0";
+    setTimeout(function () {
+        document.querySelector("#loading-cross").style.transform = "rotate(720deg)";
+        setTimeout(function () {
+            document.querySelector("#loading").style.opacity = "0";
+            setTimeout(function () {
+                document.querySelector("#loading").style.display = "none";
+            }, 350);
+        }, 3000);
+    }, 500);
+}, 500);
+
+setTimeout(function () {
+    navbar.style.transition = "transform .5s";
+    navbar.style.transform = "translateY(-80px)";
+    document.querySelector("#navbar-bg").style.transform = "translateY(-80px)";
+    document.querySelector("#main-1").style.transition = "margin-top .5s, height .5s";
+    document.querySelector("#main-1").style.marginTop = "80px";
+    document.body.style.position = "static";
+    document.querySelector("#main-1").style.height = "calc(100% - 80px)";
+    setTimeout(function () { document.querySelector("#main-2").style.opacity = "1"; }, 100);
+}, 4350);
+
+/* document.querySelector("#box-1").style.animation = "enlarge .75s forwards";
 document.querySelector("#box-1-text-container-th").style.animation = "1s white-to-green .75s forwards";
 
 document.addEventListener("visibilitychange", () => {
@@ -11,7 +45,7 @@ document.addEventListener("visibilitychange", () => {
             document.querySelector("#box-1-text-container-th").style.animation = "none";
         };
     };
-});
+}); */
 
 const main2Title = document.querySelector("#main-2-title"), main2Text = document.querySelector("#main-2-text"), verticalRect = document.querySelector("#vertical-rect"), horizontalRect = document.querySelector("#horizontal-rect"), main2Image = document.querySelector("#main-2-image")
 var scrollOffset = 100;
@@ -46,3 +80,5 @@ const handleScrollAnimation = () => {
 window.addEventListener('scroll', () => {
     handleScrollAnimation();
 });
+
+// navbar item on hover change color of other items
--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,10 @@ var lastScrollTop = 0;
 window.addEventListener("scroll", function () {
     var st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScrollTop) {
+        document.querySelector("#navbar").style.transition = "margin-top .5s";
         document.querySelector("#navbar").style.marginTop = "-80px";
     } else if (st < lastScrollTop) {
+        document.querySelector("#navbar").style.transition = "margin-top .5s";
         document.querySelector("#navbar").style.marginTop = "0";
     };
     lastScrollTop = st <= 0 ? 0 : st;
